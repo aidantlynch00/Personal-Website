0dde28f0f57f8ef80f7081e2f2710ed2db6edb37
Aidan Lynch <aidantlynch00@gmail.com>
2026-02-25 21:39:04 -0500

    Replaced static picture with a GIF for automata project

--- app/src/pages/Projects.js
+++ app/src/pages/Projects.js
@@ -21,7 +21,7 @@ const projects = [
         title: "Automata",
         time_range: "January 2025",
         link: "https://github.com/aidantlynch00/automata",
-        image_src: process.env.PUBLIC_URL + "/resources/images/automata.jpg",
+        image_src: process.env.PUBLIC_URL + "/resources/images/automata.gif",
         description: "A generic cellular automata engine. Currently supports Conway's Game of Life and a cyclic cellular automata."
     },
     {
